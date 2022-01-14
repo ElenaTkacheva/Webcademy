6fb7a62f16a3d65544e2f4f9bfbba8e5ba7e0089
Elena <79642915+ElenaTkacheva@users.noreply.github.com>
2022-01-14 13:47:06 -0500

    Add files via upload

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,14 +1,14 @@
 const navMobile = document.querySelector(".nav-mobile");
-const navTabletWr = document.querySelector(".nav-tablet-icon-wrapper");
+const navTablet = document.querySelector(".nav-tablet-icon-wrapper");
 const navTabletIcon = document.querySelector(".nav-tablet-icon");
 const navMobileList = document.querySelectorAll(".nav-mobile__list a");
 const overlay = document.querySelector(".overlay");
 
-navTabletWr.addEventListener("click", function () {
+navTablet.addEventListener("click", function () {
     this.querySelector(".nav-tablet-icon").classList.toggle("active");
 });
 
-navTabletWr.addEventListener("click", function () {
+navTablet.addEventListener("click", function () {
     navMobile.classList.toggle("nav-mobile--active");
     overlay.classList.toggle("overlay--active");
 });
