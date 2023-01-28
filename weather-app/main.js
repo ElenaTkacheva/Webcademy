deac06b8608bd5e1d514d5ebb4f1a88c3824899d
Elena <79642915+ElenaTkacheva@users.noreply.github.com>
2023-01-28 16:02:22 -0500

    Add files via upload

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,3 +1,7 @@
+import conditions from "./conditions.js";
+
+console.log(conditions);
+
 // элементы на странице
 const form = document.querySelector('.form');
 const input = document.querySelector('.input');
@@ -16,7 +20,7 @@ function showError(errorMessage) {
     header.insertAdjacentHTML("afterend", html);
 }
 
-function showCard({ name, country, temp, condition }) {
+function showCard({ name, country, temp, condition, imgPath }) {
   // отображаем полученные данные в карточке
     const html = `
                         <div class="card">
@@ -24,7 +28,7 @@ function showCard({ name, country, temp, condition }) {
 
                             <div class="card-weather">
                                 <div class="card-value">${temp}<sup>°c</sup></div>
-                                <img class="card-img" src="./img/example.png" alt="Weather">
+                                <img class="card-img" src="${imgPath}" alt="Weather">
                             </div>
 
                             <div class="card-description">${condition}</div>
@@ -44,6 +48,7 @@ async function getWeather(city) {
     return data;
 }
 
+
 /* Слушаем отправку формы */
 form.onsubmit = async (e) => {
   // отменяем отправку формы
@@ -62,11 +67,22 @@ form.onsubmit = async (e) => {
     showError(data.error.message);
   } else {
     removeCard();
+    // Get conditions
+    const info = conditions.find((obj) => obj.code === data.current.condition.code);
+    const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';
+    const fileName = (data.current.is_day ? info.day : info.night) + '.png';
+    console.log(fileName);
+    const imgPath = filePath + fileName;
+    console.log(imgPath);
+    const condition = data.current.is_day
+        ? info.languages[23]["day_text"]
+        : info.languages[23]["night_text"];
     const weatherData = {
-        name: data.location.name,
-        country: data.location.country,
-        temp: data.current.temp_c,
-        condition: data.current.condition.text
+      name: data.location.name,
+      country: data.location.country,
+      temp: data.current.temp_c,
+      condition: condition,
+      imgPath,
     };
     showCard(weatherData);
   }
